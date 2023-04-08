1367e24fa5f3cbe202c6e48f2ecd7144d4d300b8
sudarshan-doiphode <doiphode.sudarshan3@gmail.com>
2023-04-08 10:16:31 +0530

    Problem 4

---
+++ sum-between-given-range/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    private static int sumWithinRange(int first, int second){
+        return first+second;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumWithinRange(3, 2));
+    }
+}
