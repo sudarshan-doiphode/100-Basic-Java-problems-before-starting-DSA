104f26269f32c68225ae04a145e23d9225de0d5f
sudarshan-doiphode <doiphode.sudarshan3@gmail.com>
2023-04-08 10:09:18 +0530

    Problem 2

---
+++ even-or-odd-number/even/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    private static boolean isNumberEvenOrOdd(int num){
+        if(num%2==0)
+        return true;
+        else
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isNumberEvenOrOdd(3));
+    }
+}
