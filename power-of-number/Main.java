85770b7a1ccbd34aebe2d0627080b6ec5554149b
sudarshan-doiphode <doiphode.sudarshan3@gmail.com>
2023-04-10 18:28:19 +0530

    Problem 16

--- power-of-number/Main.java
+++ power-of-number/Main.java
@@ -1,14 +1,14 @@
 public class Main {
 
-    static int getPowerOfNumber(int number, int exponent){
-        int power =1;
-        while(exponent>0){
-            power *= number; 
+    static int getPowerOfNumber(int number, int exponent) {
+        int power = 1;
+        while (exponent > 0) {
+            power *= number;
             exponent--;
         }
         return power;
     }
-    
+
     public static void main(String[] args) {
         System.out.println(getPowerOfNumber(2, 3));
     }
