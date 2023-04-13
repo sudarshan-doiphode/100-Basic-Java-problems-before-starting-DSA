1dadc482160c2890536b26ad78c590eb66ffc53c
sudarshan-doiphode <doiphode.sudarshan3@gmail.com>
2023-04-13 22:14:15 +0530

    Problem 26

---
+++ area-of-circle/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+    static double areaOfCircle(int radius) {
+        double area = Math.PI * (radius * radius);
+        return area;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(areaOfCircle(7));
+    }
+
+}
