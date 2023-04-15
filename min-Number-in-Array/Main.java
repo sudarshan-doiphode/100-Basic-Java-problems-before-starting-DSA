5724baa4898087c6bdc56dc54ed6eb73766d85cd
sudarshan-doiphode <doiphode.sudarshan3@gmail.com>
2023-04-15 10:39:43 +0530

    Problem 31

---
+++ min-Number-in-Array/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    static int minNumberInArray(int[] arr) {
+        int max = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (max > arr[i])
+                max = arr[i];
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 12, 3, 14, 5, 7, 1, 14, 13, 0 };
+        System.out.println(minNumberInArray(arr));
+    }
+
+}
