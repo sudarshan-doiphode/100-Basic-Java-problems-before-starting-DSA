3222aa4b231a4c4c866dcbde8b32bf3b05fa3304
sudarshan-doiphode <doiphode.sudarshan3@gmail.com>
2023-04-21 19:19:01 +0530

    Problem 36

---
+++ sort-the-elements-of-array/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    static void sortArray(int[] arr) {
+        int temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + ",");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = { 5, 3, 7, 5, 1, 2, 0 };
+        sortArray(array);
+    }
+
+}
